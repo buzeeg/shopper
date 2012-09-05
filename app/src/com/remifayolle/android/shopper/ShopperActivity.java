62b2c45ba7f43c17c83aef9ff1edb9bc90ca3942
Rémi Fayolle <buzeeg@git>
2012-09-05 21:53:17 +0200

    Adding .android. to package and moving files

--- app/src/com/remifayolle/android/shopper/ShopperActivity.java
+++ app/src/com/remifayolle/android/shopper/ShopperActivity.java
@@ -1,4 +1,4 @@
-package com.remifayolle.shopper;
+package com.remifayolle.android.shopper;
 
 import android.app.ListActivity;
 import android.content.Intent;
--- app/src/com/remifayolle/android/shopper/ShopperDbAdapter.java
+++ app/src/com/remifayolle/android/shopper/ShopperDbAdapter.java
@@ -1,4 +1,4 @@
-package com.remifayolle.shopper;
+package com.remifayolle.android.shopper;
 
 import android.content.ContentValues;
 import android.content.Context;
